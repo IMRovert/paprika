cfee74d1b0e7a240997356a1e63991e0da8be1e7
Andrew Feltham <andrew.feltham3@gmail.com>
2018-03-06 18:35:03 -0800

    Added simple date picker and histogram for positive and negative values

--- src/pages/graphs/graphs.ts
+++ src/pages/graphs/graphs.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
+import {Chart} from 'chart.js';
+import {CalendarModal, CalendarModalOptions, CalendarResult} from "ion2-calendar";
 
 /**
  * Generated class for the GraphsPage page.
@@ -13,13 +15,82 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-graphs',
   templateUrl: 'graphs.html',
 })
+/**
+ * Using guide from https://www.joshmorony.com/adding-responsive-charts-graphs-to-ionic-2-applications/
+ */
 export class GraphsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('histogramCanvas') histogramCanvas;
+
+  histogramChart: any;
+
+  greenBackground = 'rgba(54, 162, 235, 0.2)';
+  greenBorder = 'rgba(54, 162, 235, 1)';
+  redBackground = 'rgba(255, 99, 132, 0.2)';
+  redBorder = 'rgba(255,99,132,1)';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController,) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GraphsPage');
+    this.histogramChart = new Chart(this.histogramCanvas.nativeElement, {
+
+      type: 'bar',
+      data: {
+        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        datasets: [{
+          label: 'Amount spent',
+          data: [10, 5, 1, -10, -5, 3, 3],
+          backgroundColor: [
+            this.greenBackground,
+            this.greenBackground,
+            this.greenBackground,
+            this.redBackground,
+            this.redBackground,
+            this.greenBackground,
+            this.greenBackground
+          ],
+          borderColor: [
+            this.greenBorder,
+            this.greenBorder,
+            this.greenBorder,
+            this.redBorder,
+            this.redBorder,
+            this.greenBorder,
+            this.greenBorder,
+            this.greenBorder
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+
+    });
+  }
+
+  openCalendar() {
+    const options: CalendarModalOptions = {
+      pickMode: 'range',
+      title: 'RANGE'
+    };
+
+    let myCalendar = this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss((date: { from: CalendarResult; to: CalendarResult }, type: string) => {
+      console.log(date);
+    });
   }
 
 }
